d99c6182ab44aa8be916b68e844cb833510195d2
demo <535860144@qq.com>
2019-01-02 10:04:20 +0800

    我的爱家

---
+++ config.py
@@ -0,0 +1,28 @@
+
+from redis import StrictRedis
+
+
+class Config:
+    DEBUG=None
+    SECRET_KEY = 'vY6Y2QfSEFalEmuIaywYgu3O40PtdMuyhvWpLsfyDXaej7+T/Ry7Eg=='
+
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    SESSION_TYPE='redis'
+    SESSION_REDIS= StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER= True
+    PERMAENT_SESSION_LIFETIME= 86400
+    SQLALCHEMY_DATABASE_URI= 'mysql://root:mysql@localhost/info20'
+    SQLALCHEMY_TRACK_MODIFICATIONS= False
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config_dict={
+    'development':DevelopmentConfig,
+    'production':ProductionConfig
+}
---
+++ manage.py
@@ -0,0 +1,9 @@
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from info import create_app,db
+app=create_app('development')
+manage=Manager(app)
+# Migrate(app,db)
+# manage.add_command('db',MigrateCommand)
+if __name__ == '__main__':
+    manage.run()
